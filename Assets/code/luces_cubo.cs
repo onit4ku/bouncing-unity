866798d3f08f89b4d1f3bac8e511acfd82ee00dd
onit4ku <onitaku@gmail.com>
2017-04-28 16:02:06 +0200

    initial commit

---
+++ Assets/code/luces_cubo.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class luces_cubo : MonoBehaviour
+{
+    private Rigidbody body;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+    void Awake()
+    {
+        body = gameObject.GetComponent<Rigidbody>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void CambiarLuz() {
+        Light luz = gameObject.GetComponent<Light>();
+
+        Color color = new Color();
+
+        color.r = Random.Range(0.0f, 1.0f);
+
+        color.g = Random.Range(0.0f, 1.0f);
+
+        color.b = Random.Range(0.0f, 1.0f);
+
+        luz.color = color;
+
+    }
+
+
+    void AplicarFuerza() {
+        Vector3 fuerza = new Vector3();
+
+        fuerza.x = Random.Range(-10.0f, 10.0f);                            
+        fuerza.y = Random.Range(-10.0f, 10.0f);                         
+        fuerza.z = Random.Range(-10.0f, 10.0f);
+
+        body.AddForce(fuerza, ForceMode.Impulse);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        CambiarLuz();
+        if (collision.gameObject.tag == "Suelo")
+        {
+            AplicarFuerza();
+
+        }
+    }
+}
---
+++ Assets/code/mov_recinto.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mov_recinto : MonoBehaviour {
+
+    public float speed = 10.0f;
+    public float amax = 3.5f;
+
+    private Rigidbody body;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+    void Awake()
+    {
+        body = gameObject.GetComponent<Rigidbody>();		
+    }
+
+     
+
+	// Update is called once per frame
+	void Update () {
+        Vector3 pos = body.position;
+        
+      if (Input.GetKey(KeyCode.UpArrow))
+        {
+            pos.y = pos.y + speed * Time.deltaTime;
+        }
+      if (Input.GetKey(KeyCode.DownArrow))
+        {
+            pos.y = pos.y - speed * Time.deltaTime;
+        }
+
+        body.MovePosition(pos);
+    
+	}
+}
+
+
